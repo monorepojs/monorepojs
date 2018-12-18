c71096b94de852c50772b0a20b6668e1b7840010
Joel Kanzelmeyer <kanzelm3@gmail.com>
2018-12-18 15:53:58 -0500

    fix: test script

--- packages/monorepo-scripts/config/storybook/webpack.config.js
+++ packages/monorepo-scripts/config/storybook/webpack.config.js
@@ -1,5 +1,12 @@
 'use strict'
 
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'development'
+process.env.NODE_ENV = 'development'
+
+// Ensure environment variables are read.
+require('../env')
+
 const { aliases } = require('../mono')
 const { srcPaths } = require('../paths')
 
@@ -9,6 +16,9 @@ module.exports = (baseConfig, env, defaultConfig) => {
 
   defaultConfig.module.rules[0].include = srcPaths
   defaultConfig.module.rules[0].exclude = [/[/\\\\]node_modules[/\\\\]/]
+  defaultConfig.module.rules[0].use[0].options.plugins.push(
+    '@babel/plugin-syntax-dynamic-import'
+  )
   defaultConfig.module.rules[0].use[0].options.babelrc = false
   defaultConfig.module.rules[0].use[0].options.configFile = false
 
